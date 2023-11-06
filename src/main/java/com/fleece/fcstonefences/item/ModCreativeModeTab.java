0c703ceb56edb0fe611e11c798b0da48d8d10692
FleeceComrade <fleececomrade@gmail.com>
2023-11-05 21:24:36 -0800

    1.0 Complete

--- src/main/java/com/fleece/fcstonefences/item/ModCreativeModeTab.java
+++ src/main/java/com/fleece/fcstonefences/item/ModCreativeModeTab.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab STONE_FENCES_TAB = new CreativeModeTab("Stone Fences") {
+    public static final CreativeModeTab STONE_FENCES_TAB = new CreativeModeTab("stone_fences") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModBlocks.Stone_Brick_Acacia_Fence.get());
