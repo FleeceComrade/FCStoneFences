d50dbef9f2108bee54a9f07e64a4f05b16414911
Dreu <mills1234a@gmail.com>
2025-06-23 00:06:43 -0700

    Ported to 1.20.1

--- src/main/java/com/fleece/fcstonefences/FCStoneFences.java
+++ src/main/java/com/fleece/fcstonefences/FCStoneFences.java
@@ -1,24 +1,12 @@
 package com.fleece.fcstonefences;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
-import org.jetbrains.annotations.NotNull;
 
 @Mod(FCStoneFences.MODID)
 public class FCStoneFences {
 
     public static final String MODID = "fcstonefences";
-
-    public static final CreativeModeTab STONE_FENCES_TAB = new CreativeModeTab("fc_stone_fences") {
-        @Override
-        public @NotNull ItemStack makeIcon() {
-            return new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(MODID, "stone_brick_acacia_fence")));
-        }
-    };
 
     public FCStoneFences() {
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/com/fleece/fcstonefences/Registry.java
+++ src/main/java/com/fleece/fcstonefences/Registry.java
@@ -1,11 +1,14 @@
 package com.fleece.fcstonefences;
 
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,61 +17,73 @@ import net.minecraftforge.registries.RegisterEvent;
 import java.util.Objects;
 
 import static com.fleece.fcstonefences.FCStoneFences.MODID;
-import static com.fleece.fcstonefences.FCStoneFences.STONE_FENCES_TAB;
 
 @SuppressWarnings({"DataFlowIssue", "unused"})
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MODID)
 public class Registry {
-    private static final String[] STONE_TYPES = {
-            "cobblestone",
-            "stone_brick",
-            "mossy_cobblestone",
-            "mossy_stone_brick",
-            "stone",
-            "calcite",
-            "andesite",
-            "granite",
-            "diorite",
-            "brick",
-            "prismarine",
-            "red_sandstone",
-            "mud_brick",
-            "sandstone",
-            "nether_brick",
-            "red_nether_brick",
-            "end_stone",
-            "blackstone",
-            "polished_blackstone",
-            "polished_blackstone_brick",
-            "cobbled_deepslate",
-            "polished_deepslate",
-            "deepslate_brick",
-            "deepslate_tile"
-    };
+  private static final String[] STONE_TYPES = {
+    "cobblestone",
+    "stone_brick",
+    "mossy_cobblestone",
+    "mossy_stone_brick",
+    "stone",
+    "calcite",
+    "andesite",
+    "granite",
+    "diorite",
+    "brick",
+    "prismarine",
+    "red_sandstone",
+    "mud_brick",
+    "sandstone",
+    "nether_brick",
+    "red_nether_brick",
+    "end_stone",
+    "blackstone",
+    "polished_blackstone",
+    "polished_blackstone_brick",
+    "cobbled_deepslate",
+    "polished_deepslate",
+    "deepslate_brick",
+    "deepslate_tile"
+  };
 
-    private static final String[] WOOD_TYPES = {
-            "acacia",
-            "birch",
-            "crimson",
-            "dark_oak",
-            "jungle",
-            "mangrove",
-            "oak",
-            "spruce",
-            "warped"
-    };
+  private static final String[] WOOD_TYPES = {
+    "acacia",
+    "birch",
+    "crimson",
+    "dark_oak",
+    "jungle",
+    "mangrove",
+    "oak",
+    "spruce",
+    "warped",
+    "cherry",
+    "bamboo"
+  };
 
-    @SubscribeEvent
-    public static void onRegistration(RegisterEvent event) {
-        if (Objects.equals(event.getForgeRegistry(), ForgeRegistries.BLOCKS))
+  @SubscribeEvent
+  public static void onRegistration(RegisterEvent event) {
+    if (Objects.equals(event.getForgeRegistry(), ForgeRegistries.BLOCKS))
+      for (String stone : STONE_TYPES)
+        for (String wood : WOOD_TYPES)
+          event.register(ForgeRegistries.BLOCKS.getRegistryKey(), new ResourceLocation(MODID, stone + "_" + wood + "_fence"),
+              () -> new FenceBlock(BlockBehaviour.Properties.of().strength(1.5f, 6f).requiresCorrectToolForDrops()));
+    if (event.getRegistryKey().equals(ForgeRegistries.ITEMS.getRegistryKey()))
+      for (String stone : STONE_TYPES)
+        for (String wood : WOOD_TYPES)
+          event.register(ForgeRegistries.ITEMS.getRegistryKey(), new ResourceLocation(MODID, stone + "_" + wood + "_fence"),
+              () -> new BlockItem(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(MODID, stone + "_" + wood + "_fence")), new Item.Properties()));
+    if (Objects.equals(event.getVanillaRegistry(), BuiltInRegistries.CREATIVE_MODE_TAB)) {
+      event.register(BuiltInRegistries.CREATIVE_MODE_TAB.key(), new ResourceLocation(MODID, "fc_stone_fences_tab"), () -> CreativeModeTab.builder()
+          .icon(() -> new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(MODID, "cobblestone_acacia_fence"))))
+          .title(Component.translatable("itemGroup.fc_stone_fences"))
+          .displayItems((context, output) -> {
             for (String stone : STONE_TYPES)
-                for (String wood : WOOD_TYPES)
-                    event.register(ForgeRegistries.BLOCKS.getRegistryKey(), new ResourceLocation(MODID, stone + "_" + wood + "_fence"),
-                            () -> new FenceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(1.5f, 6f).requiresCorrectToolForDrops()));
-        if (event.getRegistryKey().equals(ForgeRegistries.ITEMS.getRegistryKey()))
-            for (String stone : STONE_TYPES)
-                for (String wood : WOOD_TYPES)
-                    event.register(ForgeRegistries.ITEMS.getRegistryKey(), new ResourceLocation(MODID, stone + "_" + wood + "_fence"),
-                            () -> new BlockItem(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(MODID, stone + "_" + wood + "_fence")), new Item.Properties().tab(STONE_FENCES_TAB)));
+              for (String wood : WOOD_TYPES)
+                output.accept(ForgeRegistries.ITEMS.getValue(new ResourceLocation(MODID, stone + "_" + wood + "_fence")));
+          })
+          .build());
     }
+  }
 }
